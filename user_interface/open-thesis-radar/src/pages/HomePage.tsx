b072532ec151368c8127b1ba8173d94cf81fc290
ZeAfonso21 <joseafonsogat@gmail.com>
2026-05-06 18:45:12 +0200

    user-interface scaffold

---
+++ user_interface/open-thesis-radar/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import '../style/HomePage.css'
+
+export default function HomePage() {
+    return (
+        <main className='page'>
+            <div>
+                <span>Open Thesis Radar</span>
+            </div>
+        </main>
+    )
+}
