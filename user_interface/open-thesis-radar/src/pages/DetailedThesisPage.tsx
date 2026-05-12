a25bd9352cd85960f881484d6960dd8e5e62ef20
ZeAfonso21 <joseafonsogat@gmail.com>
2026-05-12 18:35:15 +0200

    add gitignore

--- user_interface/open-thesis-radar/src/pages/DetailedThesisPage.tsx
+++ user_interface/open-thesis-radar/src/pages/DetailedThesisPage.tsx
@@ -1,8 +1,8 @@
-import '../style/DetailedThesisPage.css'
+import '../style/DetailedThesisPage.module.css'
 
 export default function DetailedThesisPage() {
     return (
-        <main className='page'>
+        <main>
             <div>
                 <span>Detailed Open Thesis</span>
             </div>
