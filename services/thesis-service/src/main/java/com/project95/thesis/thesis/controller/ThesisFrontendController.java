51089710ce38a36694fc0c389d0f10bdbf34c871
TomasMSoares <tomassoares77@gmail.com>
2026-05-23 01:35:01 +0200

    start development of main thesis service, add scaffolding + Dockerfile + postgres container in docker-compose

---
+++ services/thesis-service/src/main/java/com/project95/thesis/thesis/controller/ThesisFrontendController.java
@@ -0,0 +1,8 @@
+package com.project95.thesis.thesis.controller;
+
+import com.project95.thesis.management.api.ApiUtil;
+import com.project95.thesis.management.dto.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+
