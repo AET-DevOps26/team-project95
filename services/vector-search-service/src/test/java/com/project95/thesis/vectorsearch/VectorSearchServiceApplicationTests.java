ebd9dc2abfd524b65c804446fb7c4075d47f1842
Martin Jardim Steinmayer <martin.j.steinmayer@gmail.com>
2026-05-22 23:07:56 +0200

    vector search first scaffold + minimal dockerfile

---
+++ services/vector-search-service/src/test/java/com/project95/thesis/vectorsearch/VectorSearchServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.project95.thesis.vectorsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.ai.openai.api-key=test")
+class VectorSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
