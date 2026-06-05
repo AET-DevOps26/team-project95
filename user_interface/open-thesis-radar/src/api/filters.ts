a13a5717027659a66b45e8fbc6d88531d9660f2b
ZeAfonso21 <joseafonsogat@gmail.com>
2026-06-05 17:10:00 +0200

    removed/commented out MOCK_THESIS logic from user interface

--- user_interface/open-thesis-radar/src/api/filters.ts
+++ user_interface/open-thesis-radar/src/api/filters.ts
@@ -1,6 +1,6 @@
 import type { operations } from '../api';
-import { USE_MOCK_THESIS } from '../config';
-import { MOCK_FILTERS } from '../mocks/theses';
+// import { USE_MOCK_THESIS } from '../config';
+// import { MOCK_FILTERS } from '../mocks/theses';
 import { defaultApiClient, type ApiClient } from './client';
 
 type AvailableFiltersResponse =
@@ -9,9 +9,9 @@ type AvailableFiltersResponse =
 export async function getAvailableFilters(
   client: ApiClient = defaultApiClient,
 ): Promise<AvailableFiltersResponse> {
-  if (USE_MOCK_THESIS) {
-    return MOCK_FILTERS;
-  }
+  // if (USE_MOCK_THESIS) {
+  //   return MOCK_FILTERS;
+  // }
 
   return client.request<AvailableFiltersResponse>('/api/v1/filters', {
     method: 'GET',
--- user_interface/open-thesis-radar/src/config.ts
+++ user_interface/open-thesis-radar/src/config.ts
@@ -1,1 +1,1 @@
-export const USE_MOCK_THESIS = import.meta.env.VITE_USE_MOCK_THESIS === 'false';
+// export const USE_MOCK_THESIS = import.meta.env.VITE_USE_MOCK_THESIS === 'true';
